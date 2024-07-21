87ce8165a43898a3f14b4c78ef8d12138f672fad
RamseyAdrian <adrianseyy@gmail.com>
2024-07-21 23:12:19 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.head')
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    @include('layouts.navbar')
+    <div class="max-w-screen-xl mx-auto">
+        <div class="flex">
+            <p>Ilmansyah Muzaddin</p>
+            <p>Ilmu Komputer</p>
+        </div>
+
+        <div class="flex gap-5">
+            <a href="#" class="" aria-current="page">About</a>
+            <a href="#" class="" aria-current="page">Experience</a>
+            <a href="#" class="" aria-current="page">Achievement</a>
+            <a href="#" class="" aria-current="page">Work Experience</a>
+        </div>
+
+        <div>
+            <div class="flex">
+                <h2>Academic</h2>
+                <div class="flex gap-5">
+                    <a href="#" class="" aria-current="page">Index</a>
+                    <a href="#" class="" aria-current="page">Draft</a>
+                    <a href="#" class="" aria-current="page">New Achievement</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
