26a94f5f265e7fc2211c96f0230fb1173b7cbd72
Nabilkarren <nabilgunner@gmail.com>
2024-07-23 14:05:33 +0700

    removed 'back to home' button from layout

--- resources/views/layouts/content-detail.blade.php
+++ resources/views/layouts/content-detail.blade.php
@@ -18,20 +18,10 @@
                     <p class="text-medium text-gray-400">Ilmu Komputer</p>
                 </div>
             </div>
-            <a href="#" class="p-2.5 text-sm bg-brand-idle hover:bg-brand-hover rounded text-white font-medium"
+            <a href="{{ route('portofolio.share') }}"
+                class="p-2.5 text-sm bg-brand-idle hover:bg-brand-hover rounded text-white font-medium"
                 aria-current="page">Share
                 Portfolio</a>
-        </div>
-
-        <div class="flex mt-12 border-b pb-5">
-            <a href="{{ route('achievement') }}"
-                class=" flex gap-2 px-2.5 py-2 text-md hover:bg-gray-200 rounded hover:font-medium"
-                aria-current="page"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="Black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M5 12h14M5 12l4-4m-4 4 4 4" />
-                </svg>
-                Back to home</a>
         </div>
 
         <div class="mt-8">
